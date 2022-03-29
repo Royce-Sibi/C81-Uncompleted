250671bdc60b54fc316ddbf6ae90dedbef90c45a
Royce Sibichan <92983553+Royce-Sibi@users.noreply.github.com>
2022-03-29 18:06:32 +0100

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,11 +1,11 @@
 canvas = document.getElementById("myCanvas");
-color = "red";
 
 ctx= canvas.getContext("2d");
+color = "red";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.linewidth = 2;
-ctx.arc(200, 200, 40 , 0, 2 * Math.PI);
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 , 0, 2*Math.PI);
 ctx.stroke();
 
 canvas.addEventListener("mousedown", my_mousedown);
@@ -26,7 +26,7 @@ function circle(mouse_x, mouse_y)
 {
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.linewidth = 2;
+    ctx.lineWidth = 2;
     ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI)
     ctx.stroke();
 }
